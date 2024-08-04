23c1fc675d84089356cf0e3e696541bb80aa19a8
benhadi-abdoussalem <dr.programme@gmail.com>
2024-08-04 19:03:24 +0100

    Adding multiples futures for getting filtered books

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,5 +1,5 @@
 const express = require('express');
-let books = require("./booksdb.js");
+const books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
@@ -19,27 +19,52 @@ public_users.get('/',function (req, res) {
 public_users.get('/isbn/:isbn',function (req, res) {
   // Extract the isbn parameter from the request URL
   const isbn = req.params.isbn;
+  // Assuming books is an object
+  const booksArray = Object.values(books);
   // Filter the books array to find books whose isbn matches the extracted isbn parameter
-  let filtered_books = books.filter((book) => book.isbn === isbn);
-  // Send the filtered_book array as the response to the client
-  res.send(filtered_books);
- });
-  
+  const filtered_books = booksArray.filter(book => book.isbn === isbn);
+  // Check if any books were found
+  if (filtered_books.length > 0) {
+    // Send the filtered_books array as the response to the client
+    res.send(filtered_books);
+  } else {
+    // Send a 404 Not Found response if no books were found
+    res.status(404).json({ message: 'No books found for the given isbn.' });
+  }
+});
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return   // Extract the email parameter from the request URL
-  const email = req.params.email;
-  // Filter the users array to find users whose email matches the extracted email parameter
-  let filtered_users = users.filter((user) => user.email === email);
-  // Send the filtered_users array as the response to the client
-  res.send(filtered_users);
+  // Extract the author parameter from the request URL
+  const author = req.params.author;
+  // Assuming books is an object
+  const booksArray = Object.values(books);
+  // Filter the books array to find books whose author matches the extracted author parameter
+  const filtered_books = booksArray.filter(book => book.author === author);
+  // Check if any books were found
+  if (filtered_books.length > 0) {
+    // Send the filtered_books array as the response to the client
+    res.send(filtered_books);
+  } else {
+    // Send a 404 Not Found response if no books were found
+    res.status(404).json({ message: 'No books found for the given author.' });
+  }
 });
-
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Extract the title parameter from the request URL
+  const title = req.params.title;
+  // Assuming books is an object
+  const booksArray = Object.values(books);
+  // Filter the books array to find books whose title matches the extracted  parameter
+  const filtered_books = booksArray.filter(book => book.title === title);
+  // Check if any books were found
+  if (filtered_books.length > 0) {
+    // Send the filtered_books array as the response to the client
+    res.send(filtered_books);
+  } else {
+    // Send a 404 Not Found response if no books were found
+    res.status(404).json({ message: 'No books found for the given title.' });
+  }
 });
 
 //  Get book review
